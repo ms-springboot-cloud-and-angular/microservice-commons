2f8d8c52fa4cd669dbea2b1833b258eb743b963c
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 14:36:07 +0200

    Create Generic Common Service

---
+++ src/main/java/com/joseluisestevez/ms/commons/services/CommonServiceImpl.java
@@ -0,0 +1,38 @@
+package com.joseluisestevez.ms.commons.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements CommonService<E> {
+
+    @Autowired
+    protected R repository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Iterable<E> findAll() {
+        return repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<E> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public E save(E entity) {
+        return repository.save(entity);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+}
