a0c7752861d7a1423c4322ba2198af65b0e97100
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 14:20:19 +0200

    New SpringBoot project commons lib

---
+++ src/test/java/com/joseluisestevez/ms/commons/MicroserviceCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
